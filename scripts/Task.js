7b2130591e5c3e74dddd0a4c3ed2986c70e60455
GabJen <gabjenramos@gmail.com>
2025-09-06 16:45:46 -0300

    feat: add Task class

---
+++ scripts/Task.js
@@ -0,0 +1,8 @@
+export default class Task {
+    constructor(title, desc, id) {
+        this.title = title;
+        this.desc = desc;
+        this.status = "pendente";
+        this.id = id
+    }
+}
